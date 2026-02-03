8c37d92712037508ec0effeb0d434baf9dbdfd8b
TitanMatyesz <taborymatyas@gmail.com>
2026-02-03 10:38:53 +0100

    hozzáadtuk az adószámot és a szabadnapok számát

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -4,5 +4,7 @@ namespace WMSProject
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string adoszam { get; } = "12345678901";
+        public int szabadnapokSzama { get; } = 15;
     }
 }
